e357ec32940aa8df98a9ddf22aac97c47381846d
Robert Woerle <robert@linuxdevelopment.de>
2016-09-30 08:44:32 +0200

    add missing tpanel-scripts recipe
    Signed-off-by: Robert Woerle <robert@linuxdevelopment.de>

---
+++ recipes-extended/tpanel/files/tpanel.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+M=2
+
+MODEL=`cat /proc/device-tree/model  | awk '{print $2}'`
+if [ "$MODEL" == "P070H111" ]
+then
+	M=3
+fi
+
+if [ "$MODEL" == "P070F111" ]
+then
+	M=4
+fi
+
+if [ "$MODEL" == "P120B110" ]
+then 
+	M=2
+fi
+
+echo $M
