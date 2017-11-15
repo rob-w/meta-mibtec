bd8df61efae479683358662b5f27ecb6c02488db
alexn <aniebling@mibtec.de>
2017-11-15 10:52:00 +0100

    add new tpanel id. read hw version of eeprom to get new tpanel hw.

--- recipes-extended/tpanel/files/tpanel.sh
+++ recipes-extended/tpanel/files/tpanel.sh
@@ -16,6 +16,12 @@ fi
 if [ "$MODEL" == "P120B110" ]
 then 
 	M=2
+	dd if=/sys/bus/i2c/devices/0-0051/eeprom of=/media/ram/temp bs=1 count=13 skip=4 >& /dev/null
+	VER=`cut -c 9-12 /media/ram/temp`
+	if [ "$VER" == "111" ]
+	then
+		M=5
+	fi
 fi
 
 echo $M
