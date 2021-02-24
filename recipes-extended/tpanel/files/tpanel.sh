1346d9dbe05bd350e0f44d3953cd1ae397b92f86
Robert Woerle <robert@linuxdevelopment.de>
2021-02-24 13:15:12 +0100

    add some missing recipes
    Signed-off-by: Robert Woerle <robert@linuxdevelopment.de>

---
+++ recipes-extended/tpanel/files/tpanel.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+M=2
+
+MODEL=`cat /proc/device-tree/model  | awk '{print $2}'`
+if [ "$MODEL" == "P070H111" ]
+then
+	M=3
+fi
+
+if [ "$MODEL" == "P070F111" ]
+then
+	M=4
+fi
+
+if [ "$MODEL" == "P120B110" ]
+then 
+	M=2
+	dd if=/sys/bus/i2c/devices/0-0051/eeprom of=/media/ram/temp bs=1 count=13 skip=4 >& /dev/null
+	VER=`cut -c 9-12 /media/ram/temp`
+	if [ "$VER" == "111" ]
+	then
+		M=5
+	fi
+fi
+
+echo $M
